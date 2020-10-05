44ed6c2a9507398fd56343a0bfe116845d7597fe
lhy9381 <li.huanyu1805@gmail.com>
2020-10-05 15:51:06 -0400

    finish up proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,114 @@
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int first_ind;
+    private int last_ind;
+
+    public ArrayDeque(){
+        // Creates an empty array deque.
+        array = (T[]) new Object[8];
+        size = 0;
+        first_ind = 0;
+        last_ind = 0;
+    }
+
+    public void resize(int capacity, Boolean is_front) {
+        T[] new_array = (T[]) new Object[capacity];
+        if (!is_front)
+            System.arraycopy(array, 0, new_array, 0, array.length);
+        else
+            System.arraycopy(array, 0, new_array, size, size);
+        array = new_array;
+    }
+
+    public void addFirst(T item) {
+        // Adds an item of type T to the front of the deque.
+        if (size==0) {
+            array[0] = item;
+        }
+        else {
+            if (first_ind==0) {
+                int capacity = size * 2;
+                resize(capacity, true);
+                first_ind = capacity - size;
+                last_ind = first_ind + size - 1;
+            }
+            array[first_ind - 1] = item;
+            first_ind--;
+        }
+        size++;
+    }
+
+    public void addLast(T item) {
+        // Adds an item of type T to the back of the deque.
+        if (size==0){
+            array[0] = item;
+        }
+        else{
+            if(last_ind==array.length-1){
+                int capacity = size * 2;
+                resize(capacity, false);
+            }
+            array[last_ind + 1] = item;
+            last_ind++;
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        // Returns true if deque is empty, false otherwise.
+        return size==0;
+    }
+
+    public int size() {
+        // Returns the number of items in the deque.
+        return size;
+    }
+
+
+    public void printDeque() {
+        // Prints the items in the deque from first to last, separated by a space.
+        if (size==0)
+            System.out.print("");
+        else {
+            int counter = first_ind;
+            while (counter < first_ind + size) {
+                String last_space = " ";
+                if (counter + 1 == first_ind + size)
+                    last_space = "";
+                System.out.print(array[counter].toString() + last_space);
+                counter++;
+            }
+        }
+    }
+
+    public T removeFirst() {
+        // Removes and returns the item at the front of the deque. If no such item exists, returns null.
+        if (size==0)
+            return null;
+        T first = array[first_ind];
+        array[first_ind] = null;
+        first_ind++;
+        size--;
+        return first;
+    }
+
+    public T removeLast() {
+        // Removes and returns the item at the back of the deque. If no such item exists, returns null.
+        if(size==0)
+            return null;
+        T last = array[last_ind];
+        array[last_ind] = null;
+        last_ind--;
+        size--;
+        return last;
+    }
+
+    public T get(int index) {
+        //  Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+        //  If no such item exists, returns null. Must not alter the deque!
+        if(size==0)
+            return null;
+        return array[first_ind + index];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,128 @@
+public class LinkedListDeque<T> {
+    public class TNode{
+        public T item;
+        public TNode next;
+        public TNode prev;
+
+        public TNode(TNode l, T i, TNode n){
+            prev = l;
+            item = i;
+            next = n;
+        }
+    }
+
+    private TNode sentinel;
+    private int size;
+
+    public LinkedListDeque(){
+        // Creates an empty linked list deque.
+        sentinel = new TNode(null, null, null);
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        // Adds an item of type T to the front of the deque.
+        if(size == 0){
+            sentinel.next = new TNode(sentinel, item, sentinel.next);
+            sentinel.next.next = sentinel;
+            sentinel.prev = sentinel.next;
+        }
+        else{
+            sentinel.next = new TNode(sentinel, item, sentinel.next);
+            sentinel.next.next.prev = sentinel.next;
+        }
+        size++;
+    }
+
+    public void addLast(T item) {
+        // Adds an item of type T to the back of the deque.
+        if(size == 0){
+            sentinel.prev = new TNode(sentinel.prev, item, sentinel);
+            sentinel.prev.prev = sentinel;
+            sentinel.next = sentinel.prev;
+        }
+        else{
+            sentinel.prev = new TNode(sentinel.prev, item, sentinel);
+            sentinel.prev.prev.next = sentinel.prev;
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        // Returns true if deque is empty, false otherwise.
+        return size==0;
+    }
+
+    public int size() {
+        // Returns the number of items in the deque.
+        return size;
+    }
+
+
+    public void printDeque() {
+        // Prints the items in the deque from first to last, separated by a space.
+        if (sentinel.next==null)
+            System.out.print("");
+        else {
+            TNode copy_first = sentinel.next;
+            while (copy_first.item != null) {
+                String last_space = " ";
+                if (copy_first.next == null)
+                    last_space = "";
+                System.out.print(copy_first.item.toString() + last_space);
+                copy_first = copy_first.next;
+            }
+        }
+    }
+
+    public T removeFirst() {
+        // Removes and returns the item at the front of the deque. If no such item exists, returns null.
+        if(sentinel.next.item == null)
+            return null;
+        T first = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return first;
+    }
+
+    public T removeLast() {
+        // TODO: Removes and returns the item at the back of the deque. If no such item exists, returns null.
+        if(sentinel.prev.item == null)
+            return null;
+        T last = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return last;
+    }
+
+    public T get(int index) {
+        //  Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+        //  If no such item exists, returns null. Must not alter the deque!
+        TNode copy_first = sentinel;
+        if (copy_first.next==null)
+            return null;
+        int counter = 0;
+        while (copy_first.next!=null){
+            if (counter == index)
+                break;
+            counter++;
+            copy_first = copy_first.next;
+        }
+        return copy_first.item;
+    }
+
+    public T getRecursive(TNode p, int index){
+        if (p.next==null)
+            return null;
+        if (index==0)
+            return p.item;
+        return getRecursive(p.next, index-1);
+    }
+
+    public T getRecursive(int index){
+        // Same as get, but uses recursion.
+        return getRecursive(sentinel, index);
+    }
+}
